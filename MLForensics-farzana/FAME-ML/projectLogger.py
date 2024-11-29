0f65cf29cc2dcab6fd1f2f38b3a479945262416d
Reid-Roberts <116679259+Reid-Roberts@users.noreply.github.com>
2024-11-28 22:23:34 -0600

    Update projectLogger.py

--- MLForensics-farzana/FAME-ML/projectLogger.py
+++ MLForensics-farzana/FAME-ML/projectLogger.py
@@ -5,7 +5,7 @@ import logging
 
 def giveMeLoggingObject():
     format_str = '%(asctime)s:%(message)s'
-    file_name  = 'W10-LOGGER.log'
+    file_name  = 'PROJECT-LOGGER.log'
     logging.basicConfig(format=format_str, filename=file_name, level=logging.INFO)
     loggerObj = logging.getLogger('simple-logger')
     return loggerObj
